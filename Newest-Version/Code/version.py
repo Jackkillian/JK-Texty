2c3c89019eae6322d9512b9a46aedd2660df8d76
Jackkillian a.k.a. Jack Freund <48269998+Jackkillian@users.noreply.github.com>
2019-05-21 08:14:50 -0600

    Create version.py

---
+++ Newest-Version/Code/version.py
@@ -0,0 +1,13 @@
+"""
+Name:    JK Texty Version File
+Version: 1.0.0
+Author:  Jackkilian a.k.a. Jack Freund
+License: MIT License
+"""
+
+# Version
+version = '1.0.0'
+# Description
+description = '1.0.0\nWhat's New:\nEverything'
+# URL to download version
+url = 'https://raw.githubusercontent.com/Jackkillian/JK-Texty-Files/master/Texty.py'
