f1008b60c62ca614311cc7e5bb3630bd9b2bdfd5
Jackkillian a.k.a. Jack Freund <48269998+Jackkillian@users.noreply.github.com>
2019-05-21 08:18:33 -0600

    Update version.py

--- Newest-Version/Code/version.py
+++ Newest-Version/Code/version.py
@@ -8,6 +8,6 @@ License: MIT License
 # Version
 version = '1.0.0'
 # Description
-description = '1.0.0\nWhat's New:\nEverything'
+description = "1.0.0\nWhat's New:\nEverything"
 # URL to download version
 url = 'https://raw.githubusercontent.com/Jackkillian/JK-Texty-Files/master/Texty.py'
