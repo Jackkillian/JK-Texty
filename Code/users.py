635e53695a3e66d72ca80e19687394515e5d4766
Jackkillian a.k.a. Jack Freund <48269998+Jackkillian@users.noreply.github.com>
2019-05-23 17:20:30 -0600

    Create users.py

---
+++ Code/users.py
@@ -0,0 +1,15 @@
+"""
+Name:     JK Texty - Code - User List
+Version:  1.0.0
+Author:   Jackkillian
+License:  MIT License
+"""
+
+# List of all users
+all_users = ['']
+
+# User To Notify
+notify_user = 'None'
+
+# What to notify
+notify_msg = 'None'
